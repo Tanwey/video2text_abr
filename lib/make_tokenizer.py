2b78739d3f2d7c448505ba25978bc7cef731b10b
robinstart <hydrojun@naver.com>
2019-08-23 15:56:47 +0900

    Refactory (1)

--- lib/make_tokenizer.py
+++
@@ -1,3 +0,0 @@
-import sentencepiece as spm
-
-spm.SentencePieceTrainer.Train('--input=../MVAD/TrainCorpus.txt --model_prefix=tokenizer --vocab_size=5000 --pad_id=0 --unk_id=3 --bos_id=1, --eos_id=2')
---
+++ utils/mask.py
@@ -0,0 +1,39 @@
+import torch
+
+
+def create_padding_mask_from_size(size, real_size):
+    '''
+        Args:
+          size: sequense length (int)
+          real_size: real sequence lenght (int)
+        Returns:
+          mask: Mask Tensor (seq)
+    assert size > real_size
+    '''
+    mask = torch.ones((size)).type(torch.uint8)
+    mask[:real_size] = 0
+    return mask
+
+
+def create_padding_mask_from_data(data):
+    '''
+        Args:
+          data: Tensor (seq)
+        Returns:
+          mask: Mask Tensor (seq)
+    '''
+    mask = (data == 0).type(torch.uint8)
+    return mask
+
+
+def create_look_ahead_mask(size):
+    '''
+        Args:
+          size: Sequence(time) size of data
+        Returns:
+          mask: Upper triangular matrix with -1e9 (size, size)
+    '''
+    mask = torch.triu(torch.ones(size, size), 1)
+    mask = mask.float().masked_fill(mask == 1, float(
+        '-1e9')).masked_fill(mask == 0, float(0.0))
+    return mask
